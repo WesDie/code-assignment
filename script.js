e7a65efd7f34aee02e104814915739c1f1e55817
WesDie <wes.dieleman@gmail.com>
2023-06-09 17:09:22 +0200

    Added: assigment 1, 2, 3

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-/** Voeg hier je code toe */
+//assigment 03: javascript:
+
+let boxes = 0;
+
+function addDivBtn() {
+    if(boxes < 10){
+        boxes++;
+
+        let box = document.createElement("div");
+        box.classList.add("block");
+
+        let boxInnerText = document.createElement("p");
+        boxInnerText.innerHTML = boxes;
+        box.appendChild(boxInnerText);
+
+        document.getElementById("assignment-03").appendChild(box);
+    } else{
+        alert("You can only add 10 boxes")
+    }
+}
